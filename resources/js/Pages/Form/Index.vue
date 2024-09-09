876cd57b43567b451ec4948caa11e99c438718d8
phoenix351 <kuat.ponimin123@gmail.com>
2024-09-09 16:05:15 +0800

    fix region_id undefined

--- resources/js/Pages/Form/Index.vue
+++ resources/js/Pages/Form/Index.vue
@@ -9,7 +9,7 @@ const formData = useRemember(
         kabupaten: null,
         kecamatan: null,
         desa: null,
-        region_id:null
+        region_id: null,
     },
     "form-data"
 );
@@ -32,11 +32,12 @@ const desa = computed({
     get: () => formData.value.desa,
     set: (value) => (formData.value.desa = value),
 });
-const region_id = computed(() => {return `71${kabupaten.value}${kecamatan.value}${desa.value}`}, 
- );
+const region_id = computed(() => {
+    return `71${kabupaten.value}${kecamatan.value}${desa.value}`;
+});
 
- const page = usePage();
- 
+const page = usePage();
+
 const destroy = (id) => {
     if (confirm("Apakah anda yakin menghapus entrian ini?")) {
         router.delete(route("form.destroy", { region_id: blok.value, id }), {
@@ -70,7 +71,6 @@ const loadDesas = async () => {
 };
 
 function submit() {
-
     router.visit(`/form/${region_id.value}`, {
         preserveState: true,
         preserveScroll: true,
@@ -88,91 +88,143 @@ function entri() {
 
 function edit(id) {
     const par = route().params;
+    // console.log({ par });
+
     // router.visit(route('form.edit', { region_id: par['region_id'], id }), {
     //     preserveState: true,
     //     preserveScroll: true,
     // });
-    window.open(
-        route("form.edit", { region_id: par["region_id"], id }),
-        "_blank"
-    );
+    // return;
+    window.open(route("form.edit", { region_id: "7106020020", id }), "_blank");
 }
-onMounted(()=>{
+onMounted(() => {
     let region = page.props.region;
     kabupatens.value = page.props.kabupatens;
-    if(region){
-        
-        kabupaten.value = region.kabupaten
-        loadKecamatans(); 
-        kecamatan.value = region.kecamatan 
-        loadDesas()
-        desa.value = region.desa 
+    if (region) {
+        kabupaten.value = region.kabupaten;
+        loadKecamatans();
+        kecamatan.value = region.kecamatan;
+        loadDesas();
+        desa.value = region.desa;
     }
-})
+});
 </script>
 
 <template>
-
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard 
+                Dashboard
             </h2>
         </template>
 
         <div class="py-8">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg py-2">
+                <div
+                    class="bg-white overflow-hidden shadow-sm sm:rounded-lg py-2"
+                >
                     <form @submit.prevent="submit">
-                        <div class="form-group flex-col grid grid-cols-1 pb-2 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div
+                            class="form-group flex-col grid grid-cols-1 pb-2 gap-x-6 gap-y-8 sm:grid-cols-6"
+                        >
                             <div class="sm:col-span-3 px-3 order-1 sm:order-1">
-                                <label for="kabupaten"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">KABUPATEN</label>
-                                <select v-model="kabupaten" name="kabupaten" @change="loadKecamatans"
+                                <label
+                                    for="kabupaten"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >KABUPATEN</label
+                                >
+                                <select
+                                    v-model="kabupaten"
+                                    name="kabupaten"
+                                    @change="loadKecamatans"
                                     class="form-control sm:col-span-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    id="kabupatens" disabled>
-                                    <option v-for="kabupaten in kabupatens" :value="kabupaten.id" :key="kabupaten.id">
+                                    id="kabupatens"
+                                    disabled
+                                >
+                                    <option
+                                        v-for="kabupaten in kabupatens"
+                                        :value="kabupaten.id"
+                                        :key="kabupaten.id"
+                                    >
                                         {{ kabupaten.nama }}
                                     </option>
                                 </select>
                             </div>
                             <div class="sm:col-span-3 px-3 order-2 sm:order-3">
-                                <label for="kecamatan"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">KECAMATAN</label>
-                                <select v-model="kecamatan" name="kecamatan"  @change="loadDesas"
+                                <label
+                                    for="kecamatan"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >KECAMATAN</label
+                                >
+                                <select
+                                    v-model="kecamatan"
+                                    name="kecamatan"
+                                    @change="loadDesas"
                                     class="form-control sm:col-span-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    id="kecamatans" disabled>
-                                    <option v-for="kecamatan in kecamatans" :value="kecamatan.id" :key="kecamatan.id">
+                                    id="kecamatans"
+                                    disabled
+                                >
+                                    <option
+                                        v-for="kecamatan in kecamatans"
+                                        :value="kecamatan.id"
+                                        :key="kecamatan.id"
+                                    >
                                         {{ kecamatan.nama }}
                                     </option>
                                 </select>
                             </div>
                             <div class="sm:col-span-3 px-3 order-3 sm:order-2">
-                                <label for="desa"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">DESA</label>
-                                <select v-model="desa" name="desa"  @change="submit"
+                                <label
+                                    for="desa"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >DESA</label
+                                >
+                                <select
+                                    v-model="desa"
+                                    name="desa"
+                                    @change="submit"
                                     class="form-control sm:col-span-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-full focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    id="desas" disabled>
-                                    <option v-for="desa in desas" :value="desa.id" :key="desa.id">
+                                    id="desas"
+                                    disabled
+                                >
+                                    <option
+                                        v-for="desa in desas"
+                                        :value="desa.id"
+                                        :key="desa.id"
+                                    >
                                         {{ desa.nama }}
                                     </option>
                                 </select>
                             </div>
-                            <div class="sm:col-span-3 px-3 order-4 sm:order-4">
-
-                            </div>
+                            <div
+                                class="sm:col-span-3 px-3 order-4 sm:order-4"
+                            ></div>
                             <div class="sm:col-span-3 px-3 order-5 sm:order-5">
-                                
-                                <button type="button" @click="submit" :disabled="!desa"
-                                    class="disabled:bg-gray-200 focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                                    <font-awesome-icon icon="fa-solid fa-rotate" class="mr-2" /> Reload Data
+                                <button
+                                    type="button"
+                                    @click="submit"
+                                    :disabled="!desa"
+                                    class="disabled:bg-gray-200 focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                                >
+                                    <font-awesome-icon
+                                        icon="fa-solid fa-rotate"
+                                        class="mr-2"
+                                    />
+                                    Reload Data
                                 </button>
-                                
-                                <button type="button" @click="entri" :disabled="!desa"
-                                    class="disabled:bg-gray-200 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                                    <font-awesome-icon icon="fa-solid fa-plus" class="mr-2" />Entri Baru
+
+                                <button
+                                    type="button"
+                                    @click="entri"
+                                    :disabled="!desa"
+                                    class="disabled:bg-gray-200 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                                >
+                                    <font-awesome-icon
+                                        icon="fa-solid fa-plus"
+                                        class="mr-2"
+                                    />Entri Baru
                                 </button>
                             </div>
                         </div>
@@ -181,14 +233,19 @@ onMounted(()=>{
                 <div class="relative mt-3 overflow-x-auto">
                     <div v-if="page.props.region != null">
                         <p>
-                            WILAYAH: [{{ page.props.region.provinsi }}{{ page.props.region.kabupaten
-                            }}{{ page.props.region.kecamatan }}{{ page.props.region.desa
-                            }}]
+                            WILAYAH: [{{ page.props.region.provinsi
+                            }}{{ page.props.region.kabupaten
+                            }}{{ page.props.region.kecamatan
+                            }}{{ page.props.region.desa }}]
                         </p>
                     </div>
 
-                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400">
+                    <table
+                        class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                    >
+                        <thead
+                            class="text-xs text-gray-700 uppercase bg-gray-300 dark:bg-gray-700 dark:text-gray-400"
+                        >
                             <tr>
                                 <th scope="col" class="px-6 py-3">
                                     No. Urut Ruta
@@ -214,15 +271,24 @@ onMounted(()=>{
                         </thead>
                         <tbody>
                             <tr v-if="page.props.data == ''">
-                                <td colspan="8" scope="row"
-                                    class="px-6 text-center py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                <td
+                                    colspan="8"
+                                    scope="row"
+                                    class="px-6 text-center py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                >
                                     Data tidak tersedia
                                 </td>
                             </tr>
-                            <tr v-else v-for="datum in page.props.data" :key="datum.id"
-                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row"
-                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            <tr
+                                v-else
+                                v-for="datum in page.props.data"
+                                :key="datum.id"
+                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                            >
+                                <th
+                                    scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                                >
                                     {{ datum.r110 }}
                                 </th>
                                 <td class="px-6 py-4">
@@ -244,15 +310,27 @@ onMounted(()=>{
                                     {{ datum.updated_at }}
                                 </td>
                                 <td class="px-6 py-4">
-                                    <button @click="edit(datum.id)" type="button" :disabled="!desa"
-                                        class="disabled:bg-gray-200 focus:outline-none text-xs font-medium text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
-                                        <font-awesome-icon icon="fa-solid fa-pencil" />
+                                    <button
+                                        @click="edit(datum.id)"
+                                        type="button"
+                                        :disabled="!desa"
+                                        class="disabled:bg-gray-200 focus:outline-none text-xs font-medium text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                                    >
+                                        <font-awesome-icon
+                                            icon="fa-solid fa-pencil"
+                                        />
                                     </button>
                                 </td>
                                 <td class="px-6 py-4">
-                                    <button @click="destroy(datum.id)" type="button" :disabled="!desa"
-                                        class="disabled:bg-gray-200 focus:outline-none text-xs font-medium text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-                                        <font-awesome-icon icon="fa-solid fa-minus" />
+                                    <button
+                                        @click="destroy(datum.id)"
+                                        type="button"
+                                        :disabled="!desa"
+                                        class="disabled:bg-gray-200 focus:outline-none text-xs font-medium text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-2 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                    >
+                                        <font-awesome-icon
+                                            icon="fa-solid fa-minus"
+                                        />
                                     </button>
                                 </td>
                             </tr>
